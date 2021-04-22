c8d200e2345c9526f94dbb3d597e79ffc883667a
paulmboyce <paul.boyce@goobito.com>
2021-04-22 13:00:27 +0100

    feat(category styling): add more catgories and include colours for dynamic
    also use TouchableNativeFeeback fro Android

--- src/data/categories.js
+++ src/data/categories.js
@@ -1,8 +1,46 @@
 const CATEGORIES = [
-	{ id: Math.random().toString(), name: "Vegan", categoryId: "CAT1" },
-	{ id: Math.random().toString(), name: "Vegetarian", categoryId: "CAT2" },
-	{ id: Math.random().toString(), name: "Keto", categoryId: "CAT3" },
-	{ id: Math.random().toString(), name: "Planet Friendly", categoryId: "CAT4" },
+	{
+		id: Math.random().toString(),
+		name: "Vegan",
+		color: "lightgreen",
+		categoryId: "CAT1",
+	},
+	{
+		id: Math.random().toString(),
+		name: "Vegetarian",
+		color: "wheat",
+		categoryId: "CAT2",
+	},
+	{
+		id: Math.random().toString(),
+		name: "Keto",
+		color: "blue",
+		categoryId: "CAT3",
+	},
+	{
+		id: Math.random().toString(),
+		name: "Planet Friendly",
+		color: "green",
+		categoryId: "CAT4",
+	},
+	{
+		id: Math.random().toString(),
+		name: "Paleo",
+		color: "pink",
+		categoryId: "CAT5",
+	},
+	{
+		id: Math.random().toString(),
+		name: "Pescatarian",
+		color: "cornflowerblue",
+		categoryId: "CAT6",
+	},
+	{
+		id: Math.random().toString(),
+		name: "Meat",
+		color: "red",
+		categoryId: "CAT7",
+	},
 ];
 
 const getCategory = (categoryId) => {
--- src/screens/CategoriesScreen.js
+++ src/screens/CategoriesScreen.js
@@ -3,10 +3,11 @@ import {
 	View,
 	Text,
 	StyleSheet,
-	Button,
 	FlatList,
 	useWindowDimensions,
 	TouchableOpacity,
+	TouchableNativeFeedback,
+	Platform,
 } from "react-native";
 
 import { ThemeStyles, Theme } from "../styles/Theme";
@@ -15,38 +16,62 @@ import { CATEGORIES } from "../data/categories";
 const CategoriesScreen = ({ navigation }) => {
 	const window = useWindowDimensions();
 
+	let Touchable = TouchableOpacity;
+	if (Platform.OS === "android" && Platform.Version >= 21) {
+		Touchable = TouchableNativeFeedback;
+	}
+
 	const styles = StyleSheet.create({
 		category: {
 			flex: 1,
-			minHeight: 80,
-			margin: 15,
-			borderColor: Theme.primaryColor,
+			margin: 12,
+			backgroundColor: Theme.backgroundColor,
 			borderWidth: 1,
-			borderRadius: 5,
-			padding: 10,
+			borderRadius: 10,
+			padding: 15,
+			minHeight: window.width * 0.3,
 			width: window.width * 0.4,
+			justifyContent: "flex-end",
+			alignItems: "flex-end",
+			shadowColor: "black",
+			shadowRadius: 10,
+			shadowOpacity: 0.26,
+			shadowOffset: { width: 0, height: 2 },
+			elevation: 3,
+			overflow: "hidden",
 		},
-		text: {
+		textRight: {
 			fontFamily: "OpenSansBold",
+			textAlign: "right",
+			fontSize: window.height < 600 ? 15 : 19,
 		},
 	});
 
 	const renderCategory = ({ index, item }) => {
 		return (
-			<TouchableOpacity
-				onPress={() => {
-					navigation.navigate({
-						routeName: "CategoryMeals",
-						params: {
-							categoryId: item.categoryId,
-						},
-					});
-				}}
-			>
-				<View style={styles.category}>
-					<Text style={styles.text}>{item.name}</Text>
-				</View>
-			</TouchableOpacity>
+			<View style={{ overflow: "hidden", borderRadius: 10 }}>
+				<Touchable
+					style={{ flex: 1 }}
+					onPress={() => {
+						navigation.navigate({
+							routeName: "CategoryMeals",
+							params: {
+								categoryId: item.categoryId,
+							},
+						});
+					}}
+				>
+					<View
+						style={{
+							...styles.category,
+							backgroundColor: item.color,
+							borderColor: item.color,
+						}}
+					>
+						<Text style={styles.textRight}>{item.name}</Text>
+					</View>
+				</Touchable>
+			</View>
 		);
 	};
 
