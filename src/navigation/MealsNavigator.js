26b720181f98bb12e2567d9558efd44893abbc67
paulmboyce <paul.boyce@goobito.com>
2021-04-26 14:50:39 +0100

    feat(themed tab icons): set icons for tabs with active color

--- src/navigation/MealsNavigator.js
+++ src/navigation/MealsNavigator.js
@@ -1,6 +1,8 @@
+import React from "react";
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import { createBottomTabNavigator } from "react-navigation-tabs";
+import { Ionicons } from "@expo/vector-icons";
 
 import CategoriesScreen from "../screens/CategoriesScreen";
 import CategoryMealsScreen from "../screens/CategoryMealsScreen";
@@ -9,6 +11,7 @@ import FavoritesScreen from "../screens/FavoritesScreen";
 import FiltersScreen from "../screens/FiltersScreen";
 
 import getNavigationOptions from "./NavigationOptions";
+import { Theme } from "../styles/Theme";
 
 const stackNavigator = createStackNavigator(
 	{
@@ -21,9 +24,33 @@ const stackNavigator = createStackNavigator(
 	}
 );
 
-const tabNavigator = createBottomTabNavigator({
-	Home: stackNavigator,
-	Settings: FiltersScreen,
-	Favorites: FavoritesScreen,
-});
+const tabNavigator = createBottomTabNavigator(
+	{
+		Home: stackNavigator,
+		Settings: FiltersScreen,
+		Favorites: FavoritesScreen,
+	},
+	{
+		defaultNavigationOptions: ({ navigation }) => ({
+			tabBarIcon: ({ focused, horizontal, tintColor }) => {
+				const { routeName } = navigation.state;
+				let iconName;
+				if (routeName === "Home") {
+					iconName = focused ? "home" : "home-outline";
+				} else if (routeName === "Settings") {
+					iconName = focused ? "settings-sharp" : "settings-outline";
+				} else if (routeName === "Favorites") {
+					iconName = focused ? "star" : "star-outline";
+				}
+
+				// You can return any component that you like here!
+				return <Ionicons name={iconName} size={25} color={tintColor} />;
+			},
+		}),
+		tabBarOptions: {
+			activeTintColor: Theme.primaryColor,
+			inactiveTintColor: "gray",
+		},
+	}
+);
 export default createAppContainer(tabNavigator);
